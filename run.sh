5e44d805ea86d326602833454fa3421924511b20
Brian Collins <bricollins@gmail.com>
2009-09-07 12:20:00 -0700

    include file in task

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ hadoop jar ./streaming.jar \
     -input articles.xml \
     -output result \
 	-inputreader 'StreamXmlRecordReader,begin=<page>,end=</page>' \
-    -mapper ~/Projects/mapreduce/map.rb \
-	-reducer ~/Projects/mapreduce/reduce.rb
+    -mapper map.rb \
+	-reducer reduce.rb \
+	-file map.rb \
+	-file reduce.rb
 	
